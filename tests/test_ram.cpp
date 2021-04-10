f220630627e2f6882f720949bd81cde65f43d68a
Paul-Jules Micolet <p.micolet@gmail.com>
2021-04-10 15:11:27 +0100

    Add basic ram test for banks

--- tests/test_ram.cpp
+++ tests/test_ram.cpp
@@ -3,25 +3,47 @@
 #include "test_utils.h"
 
 bool test_basic() {
+    // Only a single bank, 10 lines
     bool passed = true;
-    std::vector< int > trace = { 1 };
-    RAM ram = RAM( 10, trace );
+    RAM ram = RAM( 10 );
     ram[1] = std::byte{ 120 };
     
-    passed = ( ram[1] == std::byte{ 120 } );
+    passed &= ( ram[1] == std::byte{ 120 } );
     
     ram[2] = ram[1];
 
-    passed = ( ram[2] == std::byte{ 120 } );
-    passed = ( ram[1] != ram[3] );
-    passed = ( ram[1] == ram[2] );
+    passed &= ( ram[2] == std::byte{ 120 } );
+    passed &= ( ram[1] != ram[3] );
+    passed &= ( ram[1] == ram[2] );
     
+    return passed;
+}
+
+bool test_banks() {
+    bool passed = true;
+    RAM ram = RAM( 200, 2 ); // That's 2 banks each with 100 lines;
+    // We run this in debug mode which will do a check 
+#if DEBUG
+    try {
+        ram[300] = std::byte{ 69 };
+        passed &= false;
+    }
+    catch( RamException& msg ) {
+        std::cout << "Threw ! " << msg.what() << "\n";
+    }
+#endif
+
+    ram[ 0x000019 ] = std::byte{ 10 }; // Bank 0 line 25
+    ram[ 0x010032 ] = std::byte{ 10 }; // Bank 1 line 50
+
+    passed &= ram[ 0x000019 ] == ram[ 0x010032 ];
     return passed;
 }
 
 bool test_ram() {
     bool passed = true;
     RUN_TEST( test_basic() );
+    RUN_TEST( test_banks() );
     return passed;
 }
 
