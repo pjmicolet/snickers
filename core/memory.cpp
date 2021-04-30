77d84d1ef4800fbf40e5d328beb44d1e9074d524
pjmicolet <p.micolet@gmail.com>
2021-04-30 19:47:08 +0100

    Clang format memory code

--- core/memory.cpp
+++ core/memory.cpp
@@ -1,85 +1,93 @@
 #include "memory.h"
 
-RAM::RAM( size_t ramSize, int banks, Mirrors mirrors ) noexcept : banks_(banks), lines_( ramSize/banks) {
-    ram_.reserve( banks );
-    int ramLines = ramSize/banks;
-    int8_t bank = 0;
+RAM::RAM(size_t ramSize, int banks, Mirrors mirrors) noexcept
+    : banks_(banks), lines_(ramSize / banks) {
+  ram_.reserve(banks);
+  int ramLines = ramSize / banks;
+  uint8_t bank = -10;
 
-    for( int i = 0; i < banks; i++ )
-    {
-        if( mirrors.contains( i ) )
-            ram_.emplace_back( (size_t) ramLines, i, mirrors[i] );
-        else
-            ram_.emplace_back( (size_t) ramLines, i );
-    }
+  for (int i = 0; i < banks; i++) {
+    if (mirrors.contains(i))
+      ram_.emplace_back((size_t)ramLines, i, mirrors[i]);
+    else
+      ram_.emplace_back((size_t)ramLines, i);
+  }
 
-    for( auto& ramBank : ram_ ) {
-        for( int i = 0; i < ramLines; i++ )
-            ramBank.populate(i);    
-        bank++;
-    }
+  for (auto &ramBank : ram_) {
+    for (int i = 0; i < ramLines; i++)
+      ramBank.populate(i);
+    bank++;
+  }
 }
 
-RAM::RAM( size_t ramSize, std::vector<int>& tracedLines, int banks ) noexcept : banks_(banks), lines_( ramSize/banks ) {
-    ram_.reserve( banks );
-    int ramLines = ramSize/banks;
-    int tracing = 0;
-    int lines = 0;
-    int8_t bank = 0;
+RAM::RAM(size_t ramSize, std::vector<int> &tracedLines, int banks) noexcept
+    : banks_(banks), lines_(ramSize / banks) {
+  ram_.reserve(banks);
+  int ramLines = ramSize / banks;
+  int tracing = 0;
+  int lines = 0;
+  int8_t bank = 0;
 
-    for( int i = 0; i < banks; i++ )
-        ram_.emplace_back((size_t) ramLines, i );
+  for (int i = 0; i < banks; i++)
+    ram_.emplace_back((size_t)ramLines, i);
 
-    for( auto& ramBank : ram_ ) {
-        for( int i = 0; i < ramLines; i++,lines++ ) {
-            bool trace = (int)lines == tracedLines[ tracing ];
-            if( trace ) tracing++;
-            ramBank.populate(i,trace);    
-        }
-        bank++;
+  for (auto &ramBank : ram_) {
+    for (int i = 0; i < ramLines; i++, lines++) {
+      bool trace = (int)lines == tracedLines[tracing];
+      if (trace)
+        tracing++;
+      ramBank.populate(i, trace);
     }
+    bank++;
+  }
 }
 
-// Wont check mirrors, but then again we should assume mirrors were setup correctly!
-auto RAM::validateRAM( bankIndexPair& pair ) const -> void {
-    bool bankIssue = ( banks_ > 1 ) && pair.first > ( banks_ - 1 );
-    bool lineIssue = pair.second > ( lines_ - 1 );
+// Wont check mirrors, but then again we should assume mirrors were setup
+// correctly!
+auto RAM::validateRAM(bankIndexPair &pair) const -> void {
+  bool bankIssue = (banks_ > 1) && pair.first > (banks_ - 1);
+  bool lineIssue = pair.second > (lines_ - 1);
 
-    if( bankIssue || lineIssue ) {
-        std::string throwMsg = "Trying to access invalid RAM line/bank! Bank["+std::to_string( pair.first )+"]:Line["+std::to_string( pair.second )+"] only have " + std::to_string( banks_ ) + " banks and " + std::to_string( lines_ ) + " lines per bank!";
-        throw RamException( throwMsg );
-    }
+  if (bankIssue || lineIssue) {
+    std::string throwMsg = "Trying to access invalid RAM line/bank! Bank[" +
+                           std::to_string(pair.first) + "]:Line[" +
+                           std::to_string(pair.second) + "] only have " +
+                           std::to_string(banks_) + " banks and " +
+                           std::to_string(lines_) + " lines per bank!";
+    throw RamException(throwMsg);
+  }
 }
 
 // For SNES we have something like 0xBBIIII
 // where BB is the bank number and IIII is the index of the bank we're accessing
-auto RAM::calculateBank( int index ) const noexcept(DONT_THROW) -> bankIndexPair {
-    auto bank = ( index & 0xFF0000 ) >> 16;
-    auto subindex = ( index & 0xFFFF );
-    auto bankIndex = std::make_pair( bank, subindex );
+auto RAM::calculateBank(int index) const noexcept(DONT_THROW) -> bankIndexPair {
+  auto bank = (index & 0xFF0000) >> 16;
+  auto subindex = (index & 0xFFFF);
+  auto bankIndex = std::make_pair(bank, subindex);
 #if DEBUG
-    validateRAM( bankIndex );
+  validateRAM(bankIndex);
 #endif
-    return bankIndex; 
+  return bankIndex;
 }
 
-auto RAM::addressToBank( int address ) const noexcept(DONT_THROW) -> int {
-    auto bankIndex = calculateBank( address );
-    return bankIndex.first;
+auto RAM::addressToBank(int address) const noexcept(DONT_THROW) -> int {
+  auto bankIndex = calculateBank(address);
+  return bankIndex.first;
 }
 
-auto RAM::load( const int index ) noexcept(DONT_THROW) -> std::byte {
-    auto bankIndex = calculateBank( index );
-    return ram_[bankIndex.first][bankIndex.second].load();
+auto RAM::load(const int index) noexcept(DONT_THROW) -> std::byte {
+  auto bankIndex = calculateBank(index);
+  return ram_[bankIndex.first][bankIndex.second].load();
 }
 
-auto RAM::store( const int index, const std::byte data ) noexcept(DONT_THROW) -> void {
-    auto bankIndex = calculateBank( index );
-    auto mirrorBanks = ram_[bankIndex.first].bankMirrors_;
-    if( !mirrorBanks.empty() ) {
-        for( auto& bank : mirrorBanks )
-            ram_[bank][ bankIndex.second ] = data;
-    }
+auto RAM::store(const int index, const std::byte data) noexcept(DONT_THROW)
+    -> void {
+  auto bankIndex = calculateBank(index);
+  auto mirrorBanks = ram_[bankIndex.first].bankMirrors_;
+  if (!mirrorBanks.empty()) {
+    for (auto &bank : mirrorBanks)
+      ram_[bank][bankIndex.second] = data;
+  }
 
-    ram_[bankIndex.first][ bankIndex.second ] = data;
+  ram_[bankIndex.first][bankIndex.second] = data;
 }
--- core/memory.h
+++ core/memory.h
@@ -1,110 +1,125 @@
-#include <cstdint>
-#include <vector>
-#include <iostream>
-#include <cstddef>
-#include <utility>
-#include <unordered_map>
 #include "base.h"
+#include <cstddef>
+#include <cstdint>
+#include <iostream>
+#include <unordered_map>
+#include <utility>
+#include <vector>
 
 class RamException : public std::exception {
 public:
-    RamException( std::string message ) : exception_msg_( message ) {};
-    virtual const char* what() const throw() {
-        return exception_msg_.c_str();
-    }    
+  RamException(std::string message) : exception_msg_(message){};
+  virtual const char *what() const throw() { return exception_msg_.c_str(); }
+
 private:
-    std::string exception_msg_;
+  std::string exception_msg_;
 };
 
 class RAM {
 public:
-    using Mirrors = std::unordered_map< int, std::vector< int > >;
-    // Don't want a default constructor because it means very little
-    RAM() = delete;
-    RAM( size_t ramSize, int banks = 1, Mirrors mirrors = {} ) noexcept;
-    RAM( size_t ramSize, std::vector<int>& tracedLines, int banks = 1 ) noexcept;
-    RAM( size_t ramSize, std::vector<int>& tracedLines, int banks = 1, Mirrors mirrors = {} ) noexcept;
+  using Mirrors = std::unordered_map<int, std::vector<int>>;
+  // Don't want a default constructor because it means very little
+  RAM() = delete;
+  RAM(size_t ramSize, int banks = 1, Mirrors mirrors = {}) noexcept;
+  RAM(size_t ramSize, std::vector<int> &tracedLines, int banks = 1) noexcept;
+  RAM(size_t ramSize, std::vector<int> &tracedLines, int banks = 1,
+      Mirrors mirrors = {})
+  noexcept;
 
-    struct RamLine {
-        RamLine(int index) : bank_( 0 ), data_( std::byte{0} ), traced_(false), index_( index ) {};
-        RamLine(int index, int8_t bank) : bank_( bank ), data_( std::byte{0} ), traced_(false), index_( index ) {};
-        RamLine(int index, bool traced) : bank_( 0 ), data_( std::byte{0} ), traced_(false), index_( index ) {};
-        RamLine(int index, int8_t bank, bool traced) :  bank_( bank ), data_( std::byte{0} ), traced_(false), index_( index ) {};
+  struct RamLine {
+    RamLine(int index)
+        : bank_(0), data_(std::byte{0}), traced_(false), index_(index){};
+    RamLine(int index, int8_t bank)
+        : bank_(bank), data_(std::byte{0}), traced_(false), index_(index){};
+    RamLine(int index, bool traced)
+        : bank_(0), data_(std::byte{0}), traced_(false), index_(index){};
+    RamLine(int index, int8_t bank, bool traced)
+        : bank_(bank), data_(std::byte{0}), traced_(false), index_(index){};
 
-        friend auto operator<< (std::ostream& os, const RamLine& data ) -> std::ostream& {
-            return os << "RAM[" << std::hex << data.index_ << "]";
-        }
-
-        // If we hold a reference to RAM in the RamLine then we could support mirroring using the [] operator 
-        auto operator= ( std::byte data ) -> RamLine& {
-            if( traced_ )
-                std::cout << this << std::hex << " = 0x"<< std::to_integer<uint16_t>( data_ ) << " -> 0x" << std::to_integer<uint16_t>( data ) << "\n";
-            data_ = data;
-            return *this;
-        }
-
-        auto operator= ( RamLine& line ) -> RamLine& {
-            if( traced_ )
-                std::cout << this << std::hex << " = 0x"<< std::to_integer<uint16_t>( data_ ) << " -> 0x" << std::to_integer<uint16_t>( line.data_ ) << "\n";
-            data_ = line.data_;
-            return *this;
-        }
-
-        auto operator== ( std::byte data ) const -> bool {
-            return data_ == data;
-        }
-
-        auto operator== ( const RamLine& line ) const -> bool {
-            return data_ == line.data_;
-        }
-    
-        auto load() -> std::byte {
-            if( traced_ )
-                std::cout << "Loading " << this << "\n";
-            return data_;
-        }
-
-        const int8_t bank_;
-        protected:
-            std::byte data_;
-
-        private:
-            bool traced_;
-            const int index_;
-    };
-
-    struct Bank {
-        Bank( size_t numLines, int bankNum ) : bankNum_( bankNum ) { lines_.reserve(numLines); };
-        Bank( size_t numLines, int bankNum, std::vector<int> bankMirrors ) : bankMirrors_( bankMirrors ), bankNum_( bankNum ) { lines_.reserve( numLines ); }; // std::move ?
-        
-        inline void populate( const int index, bool traced = false ) {
-           lines_.emplace_back( index, bankNum_, traced );
-        }   
-        auto operator[]( int index ) -> RamLine& {
-            return lines_[index];
-        }
-
-        std::vector< int > bankMirrors_;
-        private:
-            std::vector< RamLine > lines_;
-            const int bankNum_;
-    };
-
-    auto load( const int index ) noexcept(DONT_THROW) -> std::byte;
-    auto store( const int index, const std::byte data ) noexcept(DONT_THROW) -> void;
-    auto addressToBank( int address ) const noexcept(DONT_THROW) -> int;
-
-private:
-    // Do we really need this at any point ?
-    auto operator[]( int index ) noexcept(DONT_THROW) -> RamLine& {
-        auto bankIndex = calculateBank( index );
-        return ram_[bankIndex.first][ bankIndex.second ];
+    friend auto operator<<(std::ostream &os, const RamLine &data)
+        -> std::ostream & {
+      return os << "RAM[" << std::hex << data.index_ << "]";
     }
 
-    using bankIndexPair = std::pair<int,int>;
-    auto calculateBank( int index ) const noexcept(DONT_THROW) -> bankIndexPair;
-    auto validateRAM( bankIndexPair& pair ) const -> void;
-    std::vector< Bank > ram_;
-    const int banks_;
-    const int lines_;
+    // If we hold a reference to RAM in the RamLine then we could support
+    // mirroring using the [] operator
+    auto operator=(std::byte data) -> RamLine & {
+      if (traced_)
+        std::cout << this << std::hex << " = 0x"
+                  << std::to_integer<uint16_t>(data_) << " -> 0x"
+                  << std::to_integer<uint16_t>(data) << "\n";
+      data_ = data;
+      return *this;
+    }
+
+    auto operator=(RamLine &line) -> RamLine & {
+      if (traced_)
+        std::cout << this << std::hex << " = 0x"
+                  << std::to_integer<uint16_t>(data_) << " -> 0x"
+                  << std::to_integer<uint16_t>(line.data_) << "\n";
+      data_ = line.data_;
+      return *this;
+    }
+
+    auto operator==(std::byte data) const -> bool { return data_ == data; }
+
+    auto operator==(const RamLine &line) const -> bool {
+      return data_ == line.data_;
+    }
+
+    auto load() -> std::byte {
+      if (traced_)
+        std::cout << "Loading " << this << "\n";
+      return data_;
+    }
+
+    const int8_t bank_;
+
+  protected:
+    std::byte data_;
+
+  private:
+    bool traced_;
+    const int index_;
+  };
+
+  struct Bank {
+    Bank(size_t numLines, int bankNum) : bankNum_(bankNum) {
+      lines_.reserve(numLines);
+    };
+    Bank(size_t numLines, int bankNum, std::vector<int> bankMirrors)
+        : bankMirrors_(bankMirrors), bankNum_(bankNum) {
+      lines_.reserve(numLines);
+    }; // std::move ?
+
+    inline void populate(const int index, bool traced = false) {
+      lines_.emplace_back(index, bankNum_, traced);
+    }
+    auto operator[](int index) -> RamLine & { return lines_[index]; }
+
+    std::vector<int> bankMirrors_;
+
+  private:
+    std::vector<RamLine> lines_;
+    const int bankNum_;
+  };
+
+  auto load(const int index) noexcept(DONT_THROW) -> std::byte;
+  auto store(const int index, const std::byte data) noexcept(DONT_THROW)
+      -> void;
+  auto addressToBank(int address) const noexcept(DONT_THROW) -> int;
+
+private:
+  // Do we really need this at any point ?
+  auto operator[](int index) noexcept(DONT_THROW) -> RamLine & {
+    auto bankIndex = calculateBank(index);
+    return ram_[bankIndex.first][bankIndex.second];
+  }
+
+  using bankIndexPair = std::pair<int, int>;
+  auto calculateBank(int index) const noexcept(DONT_THROW) -> bankIndexPair;
+  auto validateRAM(bankIndexPair &pair) const -> void;
+  std::vector<Bank> ram_;
+  const int banks_;
+  const int lines_;
 };
