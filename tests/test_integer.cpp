b0146334182e971a05c25c34f38636474473166b
pjmicolet <p.micolet@gmail.com>
2021-05-01 18:24:28 +0100

    Refactor Integer and add more tests

--- tests/test_integer.cpp
+++ tests/test_integer.cpp
@@ -10,6 +10,14 @@ auto test_basic_integer() -> bool {
   REQUIRE_EQUAL(small, 3);
 	small = 0xFF;
 	REQUIRE_EQUAL(small, 0xF);
+
+	Integer<12> not_as_small{};
+	not_as_small = 0b101010101101;
+	REQUIRE_EQUAL(not_as_small.bit(0), 1);
+	REQUIRE_EQUAL(not_as_small.bit(1), 0);
+
+	Integer<16> convert = small;
+	REQUIRE_EQUAL(convert, 0xF);
   return true;
 }
 
--- utils/integer/integer.h
+++ utils/integer/integer.h
@@ -12,66 +12,77 @@ template <uint8_t n> class Integer {
           std::conditional_t<n <= 32, int32_t,
                              std::conditional_t<n <= 64, int64_t, void>>>>;
 
+  constexpr auto createBitmask(uint8_t size) -> stored_type {
+    stored_type bitmask;
+    if constexpr (n == sizeof(stored_type) * 8)
+      bitmask = ~0;
+    else
+      bitmask = (1 << n) - 1;
+    return bitmask;
+  }
+
 public:
-  Integer() {
-    value = 0;
-    size = n;
-    bitmask = (1<<n)-1;
-  }
+  Integer() : bitmask_(createBitmask(n)), size_(n) { value_ = 0; }
 
-  Integer(int val) {
-    size = n;
+  Integer(int val) : bitmask_(createBitmask(n)), size_(n) {
     stored_type zero = 0;
-    bitmask = (1<<n)-1;
-    value = val & bitmask;
+    value_ = val & bitmask_;
   }
 
-  operator stored_type() const { return value; }
+  template <uint8_t size>
+  Integer(Integer<size> &other) : bitmask_(createBitmask(n)), size_(n) {
+    value_ = other;
+    value_ &= bitmask_;
+  }
+
+  operator stored_type() const { return value_; }
 
   auto operator=(const int rval) -> Integer<n> & {
-    value = static_cast<stored_type>(rval) & bitmask;
+    value_ = static_cast<stored_type>(rval) & bitmask_;
     return *this;
   }
 
   template <uint8_t otherSize>
   auto operator=(const Integer<otherSize> &rval) -> Integer<n> & {
-    value = static_cast<stored_type>(rval) & bitmask;
+    value_ = static_cast<stored_type>(rval) & bitmask_;
     return *this;
   }
 
   auto operator++(int val) -> Integer<n> {
     Integer<n> res;
-    res.value = value++;
+    res.value_ = value_++;
     return res;
   }
 
   auto operator--(int val) -> Integer<n> {
     Integer<n> res;
-    res.value = value--;
+    res.value_ = value_--;
     return res;
   }
 
   auto operator++() -> Integer<n> & {
-    ++value;
+    ++value_;
     return *this;
   }
 
   auto operator--() -> Integer<n> & {
-    --value;
+    --value_;
     return *this;
   }
 
   friend auto operator<<(std::ostream &os, const Integer &integer)
       -> std::ostream & {
-    if (integer.size <= 8) // this is stupid but it wont print if it's int8_t
-      os << static_cast<int16_t>(integer.value);
+    if (integer.size_ <= 8) // this is stupid but it wont print if it's int8_t
+      os << static_cast<int16_t>(integer.value_);
     else
-      os << integer.value;
+      os << integer.value_;
     return os;
   }
 
+  auto bit(int index) -> stored_type { return (value_ >> index) & 0x1; }
+
 private:
-  stored_type value;
-  stored_type bitmask;
-  uint8_t size;
+  stored_type value_;
+  const stored_type bitmask_;
+  const uint8_t size_;
 };
