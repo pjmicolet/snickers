e30c71b7fd9ba7067a3942b62c4e8ac6f58ac420
pjmicolet <p.micolet@gmail.com>
2021-11-06 16:03:47 +0000

    Can generate tokens that make more sense

--- factory/lexer.rs
+++ factory/lexer.rs
@@ -10,7 +10,6 @@ pub struct LexerError {
     details: String
 }
 
-
 impl LexerError {
     fn new( msg : &str ) -> LexerError {
         LexerError{ details : msg.to_string() }
@@ -23,14 +22,35 @@ impl fmt::Display for LexerError {
     }
 }
 
+pub enum TokenType {
+    ID,
+    Math,
+    OpenBrace,
+    CloseBrace,
+    OpenSqbr,
+    CloseSqbr,
+    OpenAngbr,
+    CloseAngbr
+}
+
 pub struct Token {
     str_form: String,
-    token_type : String
+    token_type : TokenType
 }
 
 impl fmt::Display for Token {
     fn fmt( &self, f : &mut fmt::Formatter ) -> fmt::Result {
-        write!( f, "str_form {}, token_type {}", self.str_form, self.token_type )
+        let ttype = match self.token_type {
+            TokenType::ID => "ID",
+            TokenType::Math => "MATH",
+            TokenType::OpenBrace => "OPEN_NRACE",
+            TokenType::CloseBrace => "CloseBrace",
+            TokenType::OpenSqbr => "OpenSqbr",
+            TokenType::CloseSqbr => "CloseSqbr",
+            TokenType::OpenAngbr => "OpenAngbr",
+            TokenType::CloseAngbr => "CloseAngbr",
+        };
+        write!( f, "str_form {}, token_type {}", self.str_form, ttype )
     }
 }
 
@@ -38,14 +58,39 @@ pub fn create_tokens( line : String ) -> Result< Vec<Token>, LexerError > {
     let potential_tokens = line.split(" ");
     let mut tokens = Vec::new();
     for p_token in potential_tokens {
+        let mut token_stack : String = "".to_string();
         for c in p_token.chars() {
-            if MATH_OPS.contains( &c ) || CHARS.contains( &c ) || OPEN_BRACKETS.contains( &c ) || CLOSED_BRACKETS.contains( &c ) {
-                continue;
+            if CHARS.contains( &c ) {
+                token_stack.push( c );
+            }
+            else if MATH_OPS.contains( &c ) {
+                if !token_stack.is_empty() {
+                    tokens.push( Token { str_form:token_stack.to_string(), token_type: TokenType::ID } );
+                    token_stack.clear();
+                }
+                tokens.push( Token { str_form:c.to_string(), token_type: TokenType::Math } );
+            }
+            else if OPEN_BRACKETS.contains( &c ) || CLOSED_BRACKETS.contains( &c ) {
+                if !token_stack.is_empty() {
+                    tokens.push( Token { str_form:token_stack.to_string(), token_type: TokenType::ID } );
+                    token_stack.clear();
+                }
+                match c {
+                    '[' => tokens.push( Token { str_form:c.to_string(), token_type: TokenType::OpenSqbr } ),
+                    '(' => tokens.push( Token { str_form:c.to_string(), token_type: TokenType::OpenBrace } ),
+                    '<' => tokens.push( Token { str_form:c.to_string(), token_type: TokenType::OpenAngbr } ),
+                    ']' => tokens.push( Token { str_form:c.to_string(), token_type: TokenType::CloseSqbr } ),
+                    ')' => tokens.push( Token { str_form:c.to_string(), token_type: TokenType::CloseBrace } ),
+                    '>' => tokens.push( Token { str_form:c.to_string(), token_type: TokenType::CloseAngbr } ),
+                    _ => return Err( LexerError::new( "Compiler should know what's avail..." ) ),
+                };
             } else {
                return Err( LexerError::new( "Incorrect synax!" ) );
             }
         }
-        tokens.push( Token{ str_form:p_token.to_string(), token_type:"bla".to_string() } );
+        if !token_stack.is_empty() {
+            tokens.push( Token{ str_form:token_stack, token_type:TokenType::ID } );
+        }
     }
     Ok( tokens )
 }
