9d2e2f811e2e35f917b3ddb9c5f7785d89ea3c6a
pjmicolet <p.micolet@gmail.com>
2021-11-06 14:23:12 +0000

    Converts the file to a vector of strings instead

--- factory/file.rs
+++ factory/file.rs
@@ -1,6 +1,11 @@
 use std::fs;
 
-pub fn open_file_as_string( file : String ) -> String {
+/// Take a file and return a vector of strings (one per line)
+pub fn open_file_as_string_vec( file : String ) -> Vec<String> {
     let data = fs::read_to_string( file ).expect( "Unable to read file" );
-    return data;
+    // Not sure this is really what I should be doing but I think it makes sense
+    // we can't make str since we don't know it at compile time, it doesn't look very fast but who
+    // knows
+    let vec = data.split("\n").map(str::to_string).collect::<Vec<String>>();//Jesus this symbol soup lol
+    return vec;
 }
--- factory/main.rs
+++ factory/main.rs
@@ -1,7 +1,9 @@
 mod file;
 fn main() {
     let path = "./test.txt";
-    let file = file::open_file_as_string( path.to_string() );
-    println!( "{}", file );
+    let file = file::open_file_as_string_vec( path.to_string() );
+    for string in file {
+        println!( "{}", string );
+    }
     println!("We bake (?) snickers here")
 }
