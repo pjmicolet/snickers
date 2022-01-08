9ed7245f051b9abc9910532a5bb6bc5442a9446c
Paul-Jules Micolet <p.micolet@gmail.com>
2022-01-08 15:28:30 +0000

    Made the integer class a number class (will have to rename later)

--- tests/test_integer.cpp
+++ tests/test_integer.cpp
@@ -31,8 +31,25 @@ auto test_basic_integer() -> bool {
   return passed;
 }
 
+auto test_basic_unsigned() -> bool {
+  bool passed = true;
+  constexpr Unsigned<4> small = 3;
+  STATIC_REQUIRE_EQUAL(small, (uint8_t)3);
+  constexpr Unsigned<4> small2 = 0xFF;
+  STATIC_REQUIRE_EQUAL(small2, 0xF);
+
+  constexpr Unsigned<12> not_as_small = 0b101010101101;
+  STATIC_REQUIRE_EQUAL(not_as_small.bit(0), 1);
+  STATIC_REQUIRE_EQUAL(not_as_small.bit(1), 0);
+
+  constexpr Unsigned<16> convert = small2;
+  STATIC_REQUIRE_EQUAL(convert, 0xF);
+  return passed;
+}
+
 auto test_integer() -> bool {
   bool passed = true;
   RUN_TEST(test_basic_integer());
+  RUN_TEST(test_basic_unsigned());
   return passed;
 }
--- utils/integer/integer.h
+++ utils/integer/integer.h
@@ -4,26 +4,32 @@
 #include <iostream>
 #include <type_traits>
 
-template <uint8_t n> class Integer {
-  using stored_type =
-      std::conditional_t<n <= 8, int8_t,
-                         std::conditional_t<n <= 16, int16_t, std::conditional_t<n <= 32, int32_t, std::conditional_t<n <= 64, int64_t, void>>>>;
+template <bool is_signed, uint8_t n> class Number {
+  using stored_type = std::conditional_t < is_signed == true,
+        std::conditional_t<n <= 8, int8_t,
+                           std::conditional_t<n <= 16, int16_t, std::conditional_t<n <= 32, int32_t, std::conditional_t<n <= 64, int64_t, void>>>>,
+        std::conditional_t<n <= 8, uint8_t,
+                           std::conditional_t<n <= 16, uint16_t, std::conditional_t<n <= 32, uint32_t, std::conditional_t<n <= 64, uint64_t, void>>>>>;
 
   constexpr auto createBitmask(uint8_t size) const -> stored_type {
     stored_type bitmask;
-    if constexpr (n == sizeof(stored_type) * 8)
-      bitmask = ~0;
+    if constexpr (n == sizeof(stored_type) * 8) {
+      if constexpr( is_signed == true )
+        bitmask = ~0;
+      else
+        bitmask = static_cast<stored_type>(~0);
+    }
     else
       bitmask = (1 << n) - 1;
     return bitmask;
   }
 
 public:
-  constexpr Integer() : bitmask_(createBitmask(n)), size_(n) { value_ = 0; }
+  constexpr Number() : bitmask_(createBitmask(n)), size_(n) { value_ = 0; }
 
-  constexpr Integer(const int val) : bitmask_(createBitmask(n)), size_(n) { value_ = static_cast<stored_type>(val & static_cast<int>(bitmask_)); }
+  constexpr Number(const int val) : bitmask_(createBitmask(n)), size_(n) { value_ = static_cast<stored_type>(val & static_cast<int>(bitmask_)); }
 
-  constexpr Integer(const Integer<n> &other) : bitmask_(createBitmask(n)), size_(n) {
+  constexpr Number(const Number<is_signed, n> &other) : bitmask_(createBitmask(n)), size_(n) {
     value_ = other._getVal();
     value_ &= bitmask_;
   }
@@ -36,46 +42,46 @@ public:
   // whatever it will expect if you're converting to a larger number then just
   // use your bitmask. both bitmasks will generated at compile time. Need to
   // test this more rigourously
-  template <uint8_t otherSize>[[nodiscard]] constexpr operator Integer<otherSize>() const noexcept {
+  template <uint8_t otherSize>[[nodiscard]] constexpr operator Number<is_signed, otherSize>() const noexcept {
     if constexpr (std::max(n, otherSize) == n)
       return value_ & createBitmask(otherSize);
     else
       return value_ & bitmask_;
   }
 
-  constexpr auto operator=(const int rval) noexcept -> Integer<n> & {
+  constexpr auto operator=(const int rval) noexcept -> Number<is_signed, n> & {
     value_ = static_cast<stored_type>(rval) & bitmask_;
     return *this;
   }
 
-  constexpr auto operator=(const Integer<n> &rval) noexcept -> Integer<n> & {
+  constexpr auto operator=(const Number<is_signed, n> &rval) noexcept -> Number<is_signed, n> & {
     value_ = static_cast<stored_type>(rval) & bitmask_;
     return *this;
   }
 
-  auto operator++(int val) -> Integer<n> {
-    Integer<n> res;
+  auto operator++(int val) -> Number<is_signed, n> {
+    Number<is_signed, n> res;
     res.value_ = value_++;
     return res;
   }
 
-  auto operator--(int val) -> Integer<n> {
-    Integer<n> res;
+  auto operator--(int val) -> Number<is_signed, n> {
+    Number<is_signed, n> res;
     res.value_ = value_--;
     return res;
   }
 
-  auto operator++() -> Integer<n> & {
+  auto operator++() -> Number<is_signed, n> & {
     ++value_;
     return *this;
   }
 
-  auto operator--() -> Integer<n> & {
+  auto operator--() -> Number<is_signed, n> & {
     --value_;
     return *this;
   }
 
-  friend auto operator<<(std::ostream &os, const Integer &integer) -> std::ostream & {
+  friend auto operator<<(std::ostream &os, const Number &integer) -> std::ostream & {
     if (integer.size_ <= 8) // this is stupid but it wont print if it's int8_t
       os << static_cast<int16_t>(integer.value_);
     else
@@ -98,15 +104,18 @@ private:
 // Need to find a way to get access to the value without it being public
 // I want it to be a friend func but I can't get the Barton Nackman trick to
 // work
-template <uint8_t n1, uint8_t n2> auto operator+(const Integer<n1> &lhs, const Integer<n2> &rhs) {
-  return Integer<std::max(n1, n2)>(lhs._getVal() + rhs._getVal());
+template <bool is_signed, uint8_t n1, uint8_t n2> auto operator+(const Number<is_signed,n1> &lhs, const Number<is_signed,n2> &rhs) {
+  return Number<is_signed, std::max(n1, n2)>(lhs._getVal() + rhs._getVal());
 }
 
 // Is actually not commutative so this is technically correct
-template <uint8_t n1, uint8_t n2> auto operator-(const Integer<n1> &lhs, const Integer<n2> &rhs) {
-  return Integer<std::max(n1, n2)>(lhs._getVal() - rhs._getVal());
+template <bool is_signed, uint8_t n1, uint8_t n2> auto operator-(const Number<is_signed,n1> &lhs, const Number<is_signed,n2> &rhs) {
+  return Number<is_signed, std::max(n1, n2)>(lhs._getVal() - rhs._getVal());
 }
 
-template <uint8_t n1, uint8_t n2> auto operator*(const Integer<n1> &lhs, const Integer<n2> &rhs) {
-  return Integer<std::max(n1, n2)>(lhs._getVal() * rhs._getVal());
+template <bool is_signed, uint8_t n1, uint8_t n2> auto operator*(const Number<is_signed,n1> &lhs, const Number<is_signed,n2> &rhs) {
+  return Number<is_signed, std::max(n1, n2)>(lhs._getVal() * rhs._getVal());
 }
+
+template<uint8_t x> using Integer = Number<true,x>;
+template<uint8_t x> using Unsigned = Number<false,x>;
