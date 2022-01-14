e050ddb3d2154d05f55766666770f478db5d9946
Paul-Jules Micolet <p.micolet@gmail.com>
2022-01-14 22:09:07 +0000

    More tests for different addressing modes
    Added some more ones, and one of the tests even has the mirroring wokring

--- tests/test_nes.cpp
+++ tests/test_nes.cpp
@@ -21,27 +21,38 @@ auto test_nes_ram_basic() -> bool {
   return passed;
 }
 
+#define CHECK_BYTE(X) REQUIRE_EQUAL(cpu.dataFetch(),X);
+
 auto test_nes_data_fetch() -> bool {
   bool passed = true;
   CPU_6502 cpu{};
   //We have an LDA with #0x33 immediate
   //LDA ZP that fetches from $10 which has 0x99
   //LDA X with base 0x1
-  std::vector<uint8_t> ram_data = {0xA9,0x33,0xA5,0x0A,0x00,0x00,0x39,0x00,0x00,0x00,0x99,0xB5,0x5};
+  std::vector<uint8_t> ram_data = {0xA9,0x33,0xA5,0x0A,0x12,0x00,0x39,0x44,0x00,0x00,0x99,0xB5,0x5,0xAD,0x07,0x00,0xBD,0x01,0x00,0xB9,0x00,0x08};
   write_ram_map(cpu,ram_data);
 
-  cpu.setPC(0);
-  auto data = cpu.dataFetch();
-  REQUIRE_EQUAL(data, 0x33);
+  cpu.setPC(0); // LDA #0x33
+  CHECK_BYTE(0x33);
 
-  cpu.setPC(2);
-  data = cpu.dataFetch();
-  REQUIRE_EQUAL(data, 0x99);
+  cpu.setPC(2); // LDA ZP (0x0A)
+  CHECK_BYTE(0x99);
 
   cpu.setPC(11);
   cpu.setX(1);
-  data = cpu.dataFetch();
-  REQUIRE_EQUAL(data, 0x39)
+  CHECK_BYTE(0x39) // LDA ZP,X (0x5)
+
+  cpu.setPC(13);
+  CHECK_BYTE(0x44) // LDA 0x0007
+
+  cpu.setPC(16);
+  cpu.setX(3);
+  CHECK_BYTE(0x12); // LDA ABS,X (0x0001)
+  cpu.setX(0);
+
+  cpu.setPC(19);
+  cpu.setY(1);
+  CHECK_BYTE(0x33); // LDA ABS,Y (0x0800) this should also test the memory banks
   return passed;
 }
 
