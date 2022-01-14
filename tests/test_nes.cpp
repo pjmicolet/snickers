5214a92811151e7e5dc3ad4f2fad2f415a5e53f8
Paul-Jules Micolet <p.micolet@gmail.com>
2022-01-14 21:50:05 +0000

    More fixes to the data fetch
    I got more things wrong

--- tests/test_nes.cpp
+++ tests/test_nes.cpp
@@ -26,7 +26,8 @@ auto test_nes_data_fetch() -> bool {
   CPU_6502 cpu{};
   //We have an LDA with #0x33 immediate
   //LDA ZP that fetches from $10 which has 0x99
-  std::vector<uint8_t> ram_data = {0xA9,0x33,0xA5,0x0A,0x00,0x00,0x00,0x00,0x00,0x00,0x99};
+  //LDA X with base 0x1
+  std::vector<uint8_t> ram_data = {0xA9,0x33,0xA5,0x0A,0x00,0x00,0x39,0x00,0x00,0x00,0x99,0xB5,0x5};
   write_ram_map(cpu,ram_data);
 
   cpu.setPC(0);
@@ -36,6 +37,11 @@ auto test_nes_data_fetch() -> bool {
   cpu.setPC(2);
   data = cpu.dataFetch();
   REQUIRE_EQUAL(data, 0x99);
+
+  cpu.setPC(11);
+  cpu.setX(1);
+  data = cpu.dataFetch();
+  REQUIRE_EQUAL(data, 0x39)
   return passed;
 }
 
